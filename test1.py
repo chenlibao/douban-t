2e26fb4d4521741256ac7ccf709825a62a4410bc
chenlibao <clb130226@163.com>
2018-08-19 22:04:49 +0800

    add test1.py

---
+++ test1.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import time
+
+
+
+
+# opt = webdriver.ChromeOptions()
+# opt.set_headless()
+# driver = webdriver.Chrome(options=opt)
+# driver.maximize_window()
+# time.sleep(3)
+# html = driver.get('https://www.baidu.com')
+# print(html)
